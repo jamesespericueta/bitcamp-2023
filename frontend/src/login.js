e243c9e6637c881385481689de7fb4d7dfcaed17
olivervillegas <olivervillegas77@icloud.com>
2023-04-08 03:46:24 -0400

    menu

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,56 +1,50 @@
 import React, {useState} from "react";
 import {BrowserRouter as Router, Routes, Route, Link, useNavigate} from "react-router-dom";
 
-function LoginScreen(){
-    const[email, setEmail] = useState("");
-    const[password, setPassword] = useState("");
-
+function LoginScreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
     const handleSubmit = (event) => {
-        event.preventDefault();
-        const form = event.target;
-        const formData = new FormData(form);
-        fetch('/script', {method: form.method, body: formData});
+      event.preventDefault();
+      const form = event.target;
+      const formData = new FormData(form);
+      fetch("/script", { method: form.method, body: formData });
     };
-
-    let navigate = useNavigate();
-    const routeChange = () =>{
-        let path = './menu.js';
-        navigate(path);
-    }
-
-    return(
-        <Router>
+  
+    return (
+      <Router>
         <div>
-            <h1>Login Screen</h1>
-            <form action="/script" onSubmit = {handleSubmit}>
-               <label>
-                Email:
-                <input
-                 type = "email"
-                 id = "username"
-                 value = {email}
-                 onChange = {(event) => setEmail(event.target.value)}
-                 />
-                </label>
-                <br />
-                <label>
-                Password:
-                <input
-                 type = "password"
-                 id = "password"
-                 value = {password}
-                 onChange = {(event) => setPassword(event.target.value)}
-                 />    
-                </label>
-                <br />
-                <button type = "submit"
-                 onClick={routeChange}
-                >Login</button> 
-            </form>
-            <script src="script.js"></script>
-        </div>
-        </Router>
+          <h1>Login Screen</h1>
+          <form action="/script" onSubmit={handleSubmit}>
+            <label>
+              Email:
+              <input
+                type="email"
+                id="username"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </label>
+            <br />
+            <Link to="/menu">
+              <button type="submit">Login</button>
+            </Link>
+          </form>
+          <script src="script.js"></script>
+        </div> 
+      </Router>
     );
-}
-
-export default LoginScreen;
+  }
+  
+  export default LoginScreen;
