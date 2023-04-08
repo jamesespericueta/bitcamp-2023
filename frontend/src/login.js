a34278268485c72a7f1dffe22be3af58ea8ad29d
Javi-L03 <javier.lopez.062003@gmail.com>
2023-04-08 15:28:30 -0400

    Created user context

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,23 +1,30 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
 import MenuScreen from "./menu"
+import { createContext } from "react";
+
+const AuthContext = createContext();
 
 function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const currentUser = useContext(AuthContext);
 
   let navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    const response = axios.post('/api/login', email, password);
     // Perform login logic here
+    const[currentUser, setCurrentUser] = useState("");
+    setCurrentUser(target.response.userID);
 
     // Navigate to the menu screen
     navigate("/menu");
   };
 
   return (
+    <AuthContext.Provider value={currentUser}>
     <div>
       <h1>Login Screen</h1>
       <form onSubmit={handleSubmit}>
@@ -42,6 +49,7 @@ function LoginScreen() {
         <button type="submit">Login</button>
       </form>
     </div>
+    </AuthContext.Provider>
   );
 }
 
