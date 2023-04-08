c2be564b4fa375fe60f0ac08a5200e988dc4ed43
olivervillegas <olivervillegas77@icloud.com>
2023-04-08 15:16:58 -0400

    v

--- frontend/src/joinGroup.js
+++ frontend/src/joinGroup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import axios from 'axios'
 
 function JoinGroupScreen(){
     /* TODO
@@ -15,15 +15,27 @@ function JoinGroupScreen(){
      *      - query db to check for code
      */ 
     const [code, setCode] = useState("");
+    const [groupExists, setGroupExists] = useState(null);
 
-    const handleCodeChange = (event) => {
+    function handleCodeChange(event) {
         setCode(event.target.value);
     };
 
-    const handleSubmit = (event) => {
+   const handleSubmit = async (event) => {
         event.preventDefault();
         // TODO: Query the database with the entered code
-      };
+        try {
+            const response = await axios.get(`/api/groups/${code}`);
+            if (response.status === 200) {
+                setGroupExists(true);
+            } else {
+                setGroupExists(false);
+            }
+          } catch (error) {
+            setGroupExists(false);
+            console.error(error);
+          }
+    };
 
     return(
         <div>
@@ -34,8 +46,11 @@ function JoinGroupScreen(){
                 Enter the join code:
                 <input type="text" value={code} onChange={handleCodeChange} />
                 </label>
-                <button type="submit">Join Group</button>
+                <button type="submit">Join Group</button>   
             </form>
+            {groupExists !== null && (
+                <p>{groupExists ? "Group exists!" : "Group does not exist!"}</p>
+            )}
         </div>
     );
 }
