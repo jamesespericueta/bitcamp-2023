2bfe3cd877276b43fc474cb83188d0a007b78340
jamesespericueta <espericueta2303@gmail.com>
2023-04-08 13:15:49 -0400

    api for groups user is part of

--- middleware/app.js
+++ middleware/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
 const session = require('express-session');
 const passport = require('./authMiddleware');
 const authRoutes = require('./authRoutes');
 const node = require('./database');
 
+dotenv.config();
+
+const pool =  new Pool({
+  connectionString: process.env.CONNECTION_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 const app = express()
-const bodyParser = require('body-parser');
-const { Pool } = require('pg');
 
 // Set up session middleware
 app.use(session({
@@ -19,7 +29,6 @@ app.use(session({
 //   Mounts passport._ functions to app request processing pipeline
 app.use(passport.initialize()); 
 app.use(passport.session());
-
 // Set up routes
 app.use('/auth', authRoutes);
 
@@ -28,6 +37,24 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Internal server error.' });
 });
+
+app.use(express.json());
+app.post('/users', async (req, res) => {
+  try{
+    const userID = req.body.userID
+    const result = await pool.query(`
+      SELECT groups.name
+      FROM groups
+      INNER JOIN groups_members ON group.id = group_members.groupid
+      WHERE group_members.user_id = $1;
+    `, [userID]);
+    const groups = result.rows.map(row => row.name);
+    res.send(groups);
+  } catch(err) {
+    console.log(err);
+    res.status(500).send("Error retrieving groups");
+  } 
+})
 
 // Connect to database and start server
 sequelize.authenticate()
