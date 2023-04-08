c8efb9bf3c94b7c9bf1a43c2bd0da0c08a766099
Javi-L03 <javier.lopez.062003@gmail.com>
2023-04-08 00:43:20 -0400

    finished login

--- login.js
+++ login.js
@@ -6,7 +6,9 @@ function LoginScreen(){
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
+        const form = event.target;
+        const formData = new FormData(form);
+        fetch('/login', {method: form.method, body: formData});
     };
 
     return(
