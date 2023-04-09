504eb7b80deaae8c78b974777429b6222dc77dd9
Javi-L03 <javier.lopez.062003@gmail.com>
2023-04-09 02:24:41 -0400

    register screen

---
+++ frontend/src/register.js
@@ -0,0 +1,71 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+const overHeader = {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
+function RegisterScreen(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUserName] = useState("");
+
+    let navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            const json = JSON.stringify({
+                "email": email,
+                "password": password,
+                "username": username
+            });
+
+            const response = await axios.post('http://localhost:8000/api/login', json, overHeader);
+
+            if(response.success){
+                navigate("/");
+            }
+        } catch(error){
+            console.error(error);
+        }
+    };
+
+    return(
+        <div>
+           <h1>Register Screen</h1>
+           <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input
+                    type="username"
+                    value={username}
+                    onChange = {(event) => setUserName(event.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Email:
+                <input
+                    type="email"
+                    value={email}
+                    onChange = {(event) => setEmail(event.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input  
+                    type="password"
+                    value={password}
+                    onChange = {(event) => setPassword(event.target.value)}
+                />
+            </label>
+            <br />
+            <button type="submit">Register</button>
+            </form> 
+        </div>
+    )
+}
